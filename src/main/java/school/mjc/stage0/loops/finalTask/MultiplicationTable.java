6e09e0d8f872b2bb5464265b8e07656bb7bf0bca
Maiko <maiko.mode05@gmail.com>
2023-09-13 14:54:54 +0400

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+       int  count = 1;
+        while(count <= 10){
+            System.out.println(count +" x " + numberTableToPrint + " = "+ count*numberTableToPrint);
+            count++;
+        }
     }
 }
